4ddb75a4757e5cb83dfaa62f458ead5eb9d71bbf
Hofsep Zakharyan <hzakharyan@nestwealth.com>
2022-01-30 15:46:21 -0500

    Asynchronously configure Dynamic Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,20 +9,22 @@ import appConfig from './utils/app.config';
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres', // type of our database
+        host: process.env.POSTGRES_HOST,
+        port: +process.env.POSTGRES_PORT,
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_NAME,
+        autoLoadEntities: true, // models will be loaded automatically
+        synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
+      }),
+    }),
     ConfigModule.forRoot({
       load: [appConfig],
     }),
     CoffeesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres', // type of our database
-      host: process.env.POSTGRES_HOST,
-      port: +process.env.POSTGRES_PORT,
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_NAME,
-      autoLoadEntities: true, // models will be loaded automatically
-      synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
-    }),
     CoffeeRatingModule,
   ],
   controllers: [AppController],
