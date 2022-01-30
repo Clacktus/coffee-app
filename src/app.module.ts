f238474272fd526782813febdf7b0ec7908826ca
Hofsep Zakharyan <hzakharyan@nestwealth.com>
2022-01-30 15:13:16 -0500

    Schema Validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,16 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.required(),
+        POSTGRES_PORT: Joi.number().default(5432),
+      }),
+    }),
     CoffeesModule,
     TypeOrmModule.forRoot({
       type: 'postgres', // type of our database
